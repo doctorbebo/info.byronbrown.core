c8277d705c1103400ba62642f26232046425a200
Byron Brown <byronrb92@gmail.com>
2022-01-27 21:30:23 -0500

    Add Observable Class

---
+++ Runtime/Observable/Observable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BeboTools
+{
+    public class ObservableObject<T>
+    {
+        private T value;
+
+        public bool HasOnChangeSubscribers => OnChange?.GetInvocationList().Length > 0;
+
+        public event Action<T> OnChange;
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnChange?.Invoke(value);
+            }
+        }
+    }
+}
