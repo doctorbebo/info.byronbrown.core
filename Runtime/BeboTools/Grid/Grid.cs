edf8f1706a9c6bd3b576b137a54225ac794d3aa5
Byron Brown <byronrb92@gmail.com>
2023-02-11 15:27:16 -0500

    Refactor Grid into "Grid" and "Grid.Helpers" classes

--- Runtime/BeboTools/Grid/Grid.cs
+++ Runtime/BeboTools/Grid/Grid.cs
@@ -1,81 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
-using BeboTools.Utils;
-using UnityEngine;
+using BeboTools.Grid.Helpers;
 
 namespace BeboTools.Grid
 {
     public class Grid : IEnumerable<Cell>
     {
-        public Cell this[Coordinates coordinates] => this[coordinates.x, coordinates.y];
-        
-        public Cell this[int x, int y] => cellArray[x, y];
+        public Cell this[Coordinates coordinates]
+        {
+            get  => this[coordinates.x, coordinates.y];
+            internal set => this[coordinates.x, coordinates.y] = value;
+        }
 
-        public int Width => cellArray.GetLength(0);
-        public int Height => cellArray.GetLength(1);
+        public Cell this[int x, int y]
+        {
+            get => CellArray[x, y];
+            internal set => CellArray[x, y] = value;
+        }
+
+        public int Width => CellArray.GetLength(0);
+        public int Height => CellArray.GetLength(1);
         
-        private Cell [,] cellArray;
+        internal Cell [,] CellArray;
 
         public Grid(int width, int height)
         {
-            cellArray = new Cell[width, height];
-            FillGrid();
-            AssignRelativeCells();
-        }
-        
-        /// <summary>
-        /// Expands the grid while maintaining the current cells within the grid
-        /// </summary>
-        /// <param name="widthIncrease"></param>
-        /// <param name="heightIncrease"></param>
-        public void Expand(int widthIncrease, int heightIncrease)
-        {
-            widthIncrease = widthIncrease > 0 ? widthIncrease : 0;
-            heightIncrease = heightIncrease > 0 ? heightIncrease : 0;
-            
-            Cell[,] newCellArray = new Cell[Width + widthIncrease, Height + heightIncrease];
-            foreach (Cell cell in this)
-            {
-                newCellArray[cell.Coordinates.x, cell.Coordinates.y] = cell;
-            }
-            cellArray = newCellArray;
-            FillGrid();
-            AssignRelativeCells();
+            CellArray = new Cell[width, height];
+            this.FillGrid();
+            this.AssignRelativeCells();
         }
 
-        /// <summary>
-        /// Returns an IEnumerable of cells from start cell to end cell (inclusive).
-        /// </summary>
-        /// <param name="startCoor"></param>
-        /// <param name="endCoor"></param>
-        /// <returns></returns>
-        public IEnumerable<Cell> CellArea(Coordinates startCoor, Coordinates endCoor)
-        {
-            Coordinates start = new Coordinates
-            (
-                startCoor.x < endCoor.x ? startCoor.x : endCoor.x,
-                startCoor.y < endCoor.y ? startCoor.y : endCoor.y
-            );
-
-            Coordinates end = new Coordinates
-            (
-                startCoor.x > endCoor.x ? startCoor.x : endCoor.x,
-                startCoor.y > endCoor.y ? startCoor.y : endCoor.y
-            );
-            
-            for (int y = start.y; y <= end.y; y++)
+        public IEnumerator<Cell> GetEnumerator()
+        { 
+            for (int y = 0; y < Height; y++)
             {
-                for (int x = start.x; x <= end.x; x++)
+                for (int x = 0; x < Width; x++)
                 {
-                    yield return cellArray[x, y];
+                    yield return CellArray[x, y];
                 }
             }
-        }
-
-        
-        public IEnumerator<Cell> GetEnumerator()
-        {
-            return IterateGrid().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -86,76 +49,6 @@ namespace BeboTools.Grid
         public override string ToString()
         {
             return $"Grid: Width: {Width} Height: {Height}";
-        }
-        
-        /// <summary>
-        /// Assigns relative cells for the grid.
-        /// </summary>
-        private void AssignRelativeCells()
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    Cell current = cellArray[x, y];
-                    current.NorthCell = GetCellAtIndex(x, y + 1); 
-                    current.EastCell = GetCellAtIndex(x + 1, y);
-                    current.SouthCell = GetCellAtIndex(x, y - 1);
-                    current.WestCell = GetCellAtIndex(x -1, y);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Attempts to get the Cell at current index. Returns null on failure
-        /// </summary>
-        /// <param name="x">x index</param>
-        /// <param name="y">y index</param>
-        /// <returns></returns>
-        private Cell GetCellAtIndex(int x, int y)
-        {
-            return ValidCoordinates(x, y) ? cellArray[x, y] : null;
-        }
-        
-        /// <summary>
-        /// Determines if the x and y are valid
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        private bool ValidCoordinates(int x, int y)
-        {
-            return x >= 0 && x < Width && y >= 0 && y < Height;
-        }
-
-        /// <summary>
-        /// Iterates through the grid filling each cell with a cell object if cell is null
-        /// </summary>
-        private void FillGrid()
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    if(cellArray[x, y].IsNull())
-                        cellArray[x, y] = new Cell(this, x, y);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Enumerates through each cell of the grid
-        /// </summary>
-        /// <returns></returns>
-        private IEnumerable<Cell> IterateGrid()
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                for (int x = 0; x < Width; x++)
-                {
-                    yield return cellArray[x, y];
-                }
-            }
         }
     }
 }
---
+++ Runtime/BeboTools/Grid/Helpers/Grid.cs
@@ -0,0 +1,110 @@
+﻿using GridClass = BeboTools.Grid.Grid;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace BeboTools.Grid.Helpers
+{
+    public static class Grid
+    {
+        /// <summary>
+        /// Determines if the x and y are valid grid coordinates
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool ValidCoordinates(this GridClass grid, int x, int y)
+        {
+            return x >= 0 && x < grid.Width && y >= 0 && y < grid.Height;
+        }
+        
+        /// <summary>
+        /// Returns an IEnumerable of cells from start cell to end cell (inclusive).
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <param name="startCoor"></param>
+        /// <param name="endCoor"></param>
+        /// <returns></returns>
+        public static IEnumerable<Cell> CellArea(this GridClass grid, Coordinates startCoor, Coordinates endCoor)
+        {
+            Coordinates start = new Coordinates
+            (
+                startCoor.x < endCoor.x ? startCoor.x : endCoor.x,
+                startCoor.y < endCoor.y ? startCoor.y : endCoor.y
+            );
+
+            Coordinates end = new Coordinates
+            (
+                startCoor.x > endCoor.x ? startCoor.x : endCoor.x,
+                startCoor.y > endCoor.y ? startCoor.y : endCoor.y
+            );
+            
+            for (int y = start.y; y <= end.y; y++)
+            {
+                for (int x = start.x; x <= end.x; x++)
+                {
+                    yield return grid[x, y];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Expands the grid while maintaining the current cells within the grid
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <param name="widthIncrease"></param>
+        /// <param name="heightIncrease"></param>
+        public static void Expand(this GridClass grid, int widthIncrease, int heightIncrease)
+        {
+            widthIncrease = widthIncrease > 0 ? widthIncrease : 0;
+            heightIncrease = heightIncrease > 0 ? heightIncrease : 0;
+
+            Cell[,] oldCellArray = grid.CellArray;
+            grid.CellArray = new Cell[grid.Width + widthIncrease, grid.Height + heightIncrease];
+
+            foreach (Cell cell in oldCellArray)
+            {
+                grid[cell.Coordinates] = cell;
+            }
+            
+            grid.FillGrid();
+            grid.AssignRelativeCells();
+        }
+        
+                
+        /// <summary>
+        /// Assigns relative cells for the grid.
+        /// </summary>
+        internal static void AssignRelativeCells(this GridClass grid)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    Cell current = grid[x, y];
+                    current.NorthCell = grid.ValidCoordinates(x, y + 1) ? grid[x, y + 1] : null; 
+                    current.EastCell = grid.ValidCoordinates(x + 1, y) ? grid[x + 1, y] : null;
+                    current.SouthCell = grid.ValidCoordinates(x, y - 1) ? grid[x, y - 1] : null; 
+                    current.WestCell = grid.ValidCoordinates(x - 1, y) ? grid[x - 1, y] : null;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Iterates through the grid filling each cell with a cell object if cell is null
+        /// </summary>
+        internal static void FillGrid(this GridClass grid)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    if (grid[x, y] == null)
+                    {
+                        grid[x, y] = new Cell(grid, x, y);
+                    }
+                }
+            }
+        }
+    }
+    
+}
