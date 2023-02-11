b775c9df5ac4a0fe1f70340065fcdb8a2c22d42e
Byron Brown <byronrb92@gmail.com>
2023-02-11 15:26:23 -0500

    Add Test folder and assembly def files

---
+++ Tests/Runtime/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests.Runtime
+{
+    public class Grid
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
