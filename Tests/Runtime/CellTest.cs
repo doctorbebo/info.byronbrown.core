0f3127cbbf9667e2e789c24664a7cdf3b6bfdc73
Byron Brown <byronrb92@gmail.com>
2023-02-11 22:35:52 -0500

    Add Grid Unit Test

---
+++ Tests/Runtime/CellTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using BeboTools.Grid;
+using BeboTools.Grid.Helpers;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CellTest
+    {
+
+        private const int StartWidth = 3;
+        private const int StartHeight = 4;
+        
+
+        private static Grid CreateDefaultGrid()
+        {
+            return new Grid(StartWidth, StartHeight);
+        }
+
+        private class TestObject
+        {
+            public string str;
+
+            public TestObject(string str)
+            {
+                this.str = str;
+            }
+        }
+        
+        [Test]
+        public void Cell_Maintains_Instance_Object()
+        {
+            Grid grid = CreateDefaultGrid(); 
+            Coordinates coordinates = new Coordinates(0, 0);
+            const string expectedString = "Test";
+            TestObject expectedObject = new TestObject(expectedString);
+            
+            grid[coordinates].SetObject(expectedObject);
+            TestObject actualObject = grid[coordinates].GetObject<TestObject>();
+            
+            Assert.AreSame(expectedObject, actualObject);
+            Assert.AreEqual(expectedObject.str, actualObject.str);
+        }
+        
+        [Test]
+        public void Cell_NorthCell()
+        {
+            Grid grid = CreateDefaultGrid();
+            
+            List<Cell> expectedCell = grid.GetRow(1).ToList();
+            List<Cell> actualCell = grid.GetRow(0).Select(c => c.NorthCell).ToList();
+            
+            CollectionAssert.AreEqual(expectedCell, actualCell);
+        }
+        
+        [Test]
+        public void Cell_EastCell()
+        {
+            Grid grid = CreateDefaultGrid();
+            
+            List<Cell> expectedCell = grid.GetColumn(1).ToList();
+            List<Cell> actualCell = grid.GetColumn(0).Select(c => c.EastCell).ToList();
+            
+            CollectionAssert.AreEqual(expectedCell, actualCell);
+        }
+        
+        [Test]
+        public void Cell_SouthCell()
+        {
+            Grid grid = CreateDefaultGrid();
+            
+            List<Cell> expectedCell = grid.GetRow(0).ToList();
+            List<Cell> actualCell = grid.GetRow(1).Select(c => c.SouthCell).ToList();
+            
+            CollectionAssert.AreEqual(expectedCell, actualCell);
+        }
+        
+        [Test]
+        public void Cell_WestCell()
+        {
+            Grid grid = CreateDefaultGrid();
+            
+            List<Cell> expectedCell = grid.GetColumn(0).ToList();
+            List<Cell> actualCell = grid.GetColumn(1).Select(c => c.WestCell).ToList();
+            
+            CollectionAssert.AreEqual(expectedCell, actualCell);
+        }
+    }
+}
---
+++ Tests/Runtime/GridTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using BeboTools.Grid;
+using BeboTools.Grid.Helpers;
+using UnityEngine.TestTools;
+
+namespace Tests.Runtime
+{
+    public class GridTest
+    {
+        private const int StartWidth = 3;
+        private const int StartHeight = 4;
+
+        private static Grid CreateDefaultGrid()
+        {
+            return new Grid(StartWidth, StartHeight);
+        }
+        
+        [Test]
+        public void Grid_CellArea()
+        {
+            Grid grid = CreateDefaultGrid();
+            const int start = 1;
+            const int end = 2;
+
+            List<Cell> expectedCells = new List<Cell>()
+            {
+                grid[1,1],
+                grid[2,1],
+                grid[1,2],
+                grid[2,2]
+            };
+            
+
+            Coordinates a = new Coordinates(start, start);
+            Coordinates b = new Coordinates(end, end);
+            List<Cell> actualCellsells = grid.CellArea(a, b).ToList();
+
+            Assert.AreEqual(expectedCells.Count, actualCellsells.Count);
+            
+            for (int i = 0; i < expectedCells.Count; i++)
+            {
+                Assert.AreSame(expectedCells[i], actualCellsells[i]);
+            }
+        }
+
+        [Test]
+        public void Expand_Grid()
+        {
+            Grid grid = CreateDefaultGrid();
+
+            int oldWidth = grid.Width; 
+            int oldHeight = grid.Height; 
+
+            const int widthIncrease = 1;
+            const int heightIncrease = 1;
+                
+            
+            grid.Expand(widthIncrease, heightIncrease);
+            
+            Assert.AreEqual((widthIncrease + oldWidth) * (heightIncrease + oldHeight), grid.Count());
+        }
+        
+        [Test]
+        public void Expand_Grid_Cell_Instances_Are_Maintained()
+        {
+            Grid grid = CreateDefaultGrid();
+            List<Cell> expectedCells = grid.ToList();
+            (Coordinates start, Coordinates end) coor = (new Coordinates(0, 0), new Coordinates(StartWidth -1, StartHeight -1));
+            
+
+            const int widthIncrease = 1;
+            const int heightIncrease = 1;
+            grid.Expand(widthIncrease, heightIncrease);
+
+            CollectionAssert.AreEqual(expectedCells, grid.CellArea(coor.start, coor.end));
+        }
+
+        [Test]
+        public void Grid_ValidateCooridinates()
+        {
+            Grid grid = CreateDefaultGrid();
+
+            Assert.IsFalse(grid.ValidCoordinates(-1, 0));
+            Assert.IsFalse(grid.ValidCoordinates(0, -1));
+            Assert.IsFalse(grid.ValidCoordinates(StartWidth, 0));
+            Assert.IsFalse(grid.ValidCoordinates(0, StartHeight));
+
+            for (int x = 0; x < StartWidth; x++)
+            {
+                for (int y = 0; y < StartHeight; y++)
+                {
+                    Assert.IsTrue(grid.ValidCoordinates(x, y));
+                }
+                
+            }
+        }
+
+        [Test]
+        public void Grid_GetGridRow()
+        {
+            Grid grid = CreateDefaultGrid();
+            const int cellRowIndex = 0;
+            
+            List<Cell> expectedCellRow = new List<Cell>();
+            for (int x = 0; x < StartWidth; x++)
+            {
+                expectedCellRow.Add(grid[x, cellRowIndex]);       
+            }
+            
+            CollectionAssert.AreEqual(expectedCellRow, grid.GetRow(cellRowIndex));
+        }
+        
+        [Test]
+        public void Grid_GetGridColumn()
+        {
+            Grid grid = CreateDefaultGrid();
+            const int cellColumnIndex = 0;
+                
+            List<Cell> expectedCellColmn = new List<Cell>();
+            for (int y = 0; y < StartHeight; y++)
+            {
+                expectedCellColmn.Add(grid[cellColumnIndex, y]);       
+            }
+            
+            CollectionAssert.AreEqual(expectedCellColmn, grid.GetColumn(cellColumnIndex));
+        }
+        
+    }
+}
